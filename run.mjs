593f829933812f5377b41317aa1be2c12034936f
Leonid Kadantsev <leonid.cordon@gmail.com>
2026-07-13 03:31:29 +0300

    13_07

--- run.mjs
+++
@@ -1,8 +0,0 @@
-import { spawnSync } from 'child_process';
-import { readFileSync } from 'fs';
-const json = readFileSync('temp.json', 'utf8').trim();
-
-let res = spawnSync('node', ['dev/generator/registry-tool.mjs', 'validate', json], { stdio: 'inherit', shell: false });
-if (res.status === 0) {
-  spawnSync('node', ['dev/generator/registry-tool.mjs', 'insert', 'best-eco-resorts-tulum-sustainable-luxury', json], { stdio: 'inherit', shell: false });
-}
